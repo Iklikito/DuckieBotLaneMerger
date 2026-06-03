6e0edbd50e2ddaceb1b217f5b3aafc1ad22a9284
Iklikito <iklikito2@gmail.com>
2026-06-04 00:11:39 +0400

    Added a map for simulation

--- tasks/introduction/packages/manual_drive.py
+++ tasks/introduction/packages/manual_drive.py
@@ -2,9 +2,27 @@ from typing import Dict, Tuple
 import logging
 logger = logging.getLogger(__name__)
 
-SPEED = 1
-TURN = 0.5
+SPEED = 0.5
+TURN = 0.3
 
 
 def get_motor_speeds(keys_pressed: Dict[str, bool]) -> Tuple[float, float]:
-    pass
+    left, right = 0, 0
+
+    if keys_pressed.get('up', False):
+        left  += SPEED
+        right += SPEED
+
+    if keys_pressed.get('down', False):
+        left  -= SPEED
+        right -= SPEED
+
+    if keys_pressed.get('left', False):
+        left  -= TURN
+        right += TURN
+
+    if keys_pressed.get('right', False):
+        left  += TURN
+        right -= TURN
+        
+    return left, right
